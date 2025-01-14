2722162613d11d4b66ac0504f339703bb0b90f05
stanislike <s.joulah@outlook.fr>
2025-01-14 03:46:12 +0100

    video good, home finished

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,21 +1,56 @@
 import React from "react";
-import ReactPlayer from "react-player";
-import MyVideo from "../assets/video/test.mp4";
+import { useRef } from "react";
+// import ReactPlayer from "react-player";
+// import MyVideo from "../assets/video/test.mp4";
 
 const Video = () => {
+  const videoRef = useRef(null);
+
+  const handlePlay = () => {
+    console.log("play button clicked");
+
+    const video = videoRef.current;
+    console.log(video);
+
+    if (video) {
+      video
+        .play()
+        .then(() => {
+          if (video.requestFullscreen) {
+            video.requestFullscreen();
+          } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+          } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+          } else if (video.msrequestFullscreen) {
+            video.msrequestFullscreen();
+          }
+        })
+        .catch((err) => console.error("Erreur pendant la lecture :", err));
+    }
+  };
+
   return (
     <div className="video-section">
       <div className="video-container">
-        <ReactPlayer url={MyVideo} controls className="react-player" />
-      </div>
-      <div className="text-container">
-        <h2>Voir ma vidéo</h2>
-        <p>
-          many variations of passages of Lorem Ipsum available, but the majority
-          have suffered alteration in some form, by injected humour, or
-          randomised words which
-        </p>
-        <div className="play-icon"></div>
+        <video
+          ref={videoRef}
+          className="video"
+          preload="auto"
+          onError={() => console.error("Erreur lors du chargement de la vidéo")}
+        >
+          <source src="/video/test.mp4" />
+          Votre navigateur ne supporte pas la vidéo.
+        </video>
+        <div className="text-container">
+          <h2>Voir ma vidéo</h2>
+          <p>
+            Many variations of passages of Lorem Ipsum available, but the
+            majority have suffered alteration in some form, by injected humour,
+            or randomised words which
+          </p>
+        </div>
+        <button className="play-button" onClick={handlePlay} />
       </div>
     </div>
   );
